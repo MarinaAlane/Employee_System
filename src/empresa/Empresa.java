165259dd767e1f0d882705e401170fbb573f0016
Marina Alane <marina.alane@outlook.com>
2025-02-10 10:25:45 -0300

    chore: separa metodos e adiciona formatacao

--- src/empresa/Empresa.java
+++ src/empresa/Empresa.java
@@ -1,64 +1,93 @@
 package empresa;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Empresa {
-  public static void main(String[] args) {
-    List<Funcionario> funcionarios = new ArrayList<>();
 
-    Pessoa pessoaMaria = new Pessoa("Maria", LocalDate.of(2000, 10, 18));
-    Funcionario funcionariaMaria = new Funcionario(pessoaMaria, new BigDecimal("2009.44"), "Operador");
-    funcionarios.add(funcionariaMaria);
+  private static void addPessoa(String nome, LocalDate dataDeNascimento) {
+    Pessoa pessoa = new Pessoa(nome, dataDeNascimento);
+    pessoas.add(pessoa);
 
-    Pessoa pessoaJoao = new Pessoa("João", LocalDate.of(1990, 05, 12));
-    Funcionario funcionarioJoao = new Funcionario(pessoaJoao, new BigDecimal("2284.38"), "Operador");
-    funcionarios.add(funcionarioJoao);
+    System.out.println("Pessoa adicionada: " + pessoa);
+  }
 
-    Pessoa pessoaCaio = new Pessoa("Caio", LocalDate.of(1961, 05, 02));
-    Funcionario funcionarioCaio = new Funcionario(pessoaCaio, new BigDecimal("9836.14"), "Coordenador");
-    funcionarios.add(funcionarioCaio);
-
-    Pessoa pessoaMiguel = new Pessoa("Miguel", LocalDate.of(1988, 10, 14));
-    Funcionario funcionarioMiguel = new Funcionario(pessoaMiguel, new BigDecimal("19119.88"), "Diretor");
-    funcionarios.add(funcionarioMiguel);
-
-    Pessoa pessoaHeitor = new Pessoa("Heitor", LocalDate.of(1999, 11, 19));
-    Funcionario funcionarioHeitor = new Funcionario(pessoaHeitor, new BigDecimal("1582.72"), "Operador");
-    funcionarios.add(funcionarioHeitor);
-
-    Pessoa pessoaAlice = new Pessoa("Alice", LocalDate.of(1995, 01, 05));
-    Funcionario funcionariaAlice = new Funcionario(pessoaAlice, new BigDecimal("9836.14"), "Recepcionista");
-    funcionarios.add(funcionariaAlice);
-
-    Pessoa pessoaArthur = new Pessoa("Arthur", LocalDate.of(1993, 03, 31));
-    Funcionario funcionarioArthur = new Funcionario(pessoaArthur, new BigDecimal("4071.84"), "Contador");
-    funcionarios.add(funcionarioArthur);
-
-    Pessoa pessoaLaura = new Pessoa("Laura", LocalDate.of(1994, 07, 8));
-    Funcionario funcionariaLaura = new Funcionario(pessoaLaura, new BigDecimal("3017.45"), "Gerente");
-    funcionarios.add(funcionariaLaura);
-
-    Pessoa pessoaHeloísa = new Pessoa("Heloísa", LocalDate.of(2003, 05, 24));
-    Funcionario funcionariaHeloísa = new Funcionario(pessoaHeloísa, new BigDecimal("1606.85"), "Eletricista");
-    funcionarios.add(funcionariaHeloísa);
-
-    Pessoa pessoaHelena = new Pessoa("Helena", LocalDate.of(1996, 9, 02));
-    Funcionario funcionariaHelena = new Funcionario(pessoaHelena, new BigDecimal("2799.93"), "Gerente");
-    funcionarios.add(funcionariaHelena);
-
-    System.out.println("Lista de Funcionários:");
-    for (Funcionario funcionario : funcionarios) {
-      System.out.println(funcionario);
+  private static void addFuncionario(String nome, BigDecimal salario, String funcao) {
+    for (Pessoa pessoa : pessoas) {
+      Funcionario funcionario = new Funcionario(pessoa, salario, funcao);
+      funcionarios.add(funcionario);
     }
+  }
 
-    funcionarios.removeIf(funcionario -> funcionario.getPessoa().getNome().equals("João"));
+  private static List<Pessoa> pessoas = new ArrayList<>();
+
+  private static void excluiFuncionario(String nome) {
+    funcionarios.removeIf(funcionario -> funcionario.getPessoa().getNome().equals(nome));
 
     System.out.println("Lista de Funcionários após a exclusão:");
     for (Funcionario funcionario : funcionarios) {
       System.out.println(funcionario);
+    }
+  }
+
+  private static void formatarFuncionario(Funcionario funcionario) {
+    Pessoa pessoa = funcionario.getPessoa();
+    String nome = pessoa.getNome();
+    String dataNascimento = pessoa.getDataDeNascimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    String salario = "R$ " + funcionario.getSalario().setScale(2, RoundingMode.HALF_EVEN).toString().replace(".", ",");
+
+    System.out.println(String.format("Nome: %s, Data de Nascimento: %s, Salário: %s, Cargo: %s", nome, dataNascimento,
+        salario, funcionario.getFuncao()));
+  }
+
+  private static List<Funcionario> funcionarios = new ArrayList<>();
+
+  public static void main(String[] args) {
+
+    addPessoa("Maria", LocalDate.of(2000, 10, 18));
+    addFuncionario("Maria", new BigDecimal("2009.44"), "Operador");
+
+    addPessoa("João", LocalDate.of(1990, 05, 12));
+    addFuncionario("João", new BigDecimal("2284.38"), "Operador");
+
+    addPessoa("Caio", LocalDate.of(1961, 05, 02));
+    addFuncionario("Caio", new BigDecimal("9836.14"), "Coordenador");
+
+    addPessoa("Miguel", LocalDate.of(1988, 10, 14));
+    addFuncionario("Miguel", new BigDecimal("19119.88"), "Diretor");
+
+    addPessoa("Heitor", LocalDate.of(1999, 11, 19));
+    addFuncionario("Heitor", new BigDecimal("1582.72"), "Operador");
+
+    addPessoa("Alice", LocalDate.of(1995, 01, 05));
+    addFuncionario("Alice", new BigDecimal("9836.14"), "Recepcionista");
+
+    addPessoa("Arthur", LocalDate.of(1993, 03, 31));
+    addFuncionario("Arthur", new BigDecimal("4071.84"), "Contador");
+
+    addPessoa("Laura", LocalDate.of(1994, 07, 8));
+    addFuncionario("Laura", new BigDecimal("3017.45"), "Gerente");
+
+    addPessoa("Heloísa", LocalDate.of(2003, 05, 24));
+    addFuncionario("Heloísa", new BigDecimal("1606.85"), "Eletricista");
+
+    addPessoa("Helena", LocalDate.of(1996, 9, 02));
+    addFuncionario("Helena", new BigDecimal("2799.93"), "Gerente");
+
+    System.out.println("Lista de Funcionários:");
+    for (Funcionario func : funcionarios) {
+      System.out.println(func);
+    }
+
+    excluiFuncionario("João");
+
+    System.out.println("Lista de funcionários formatada:");
+    for (Funcionario funcionario : funcionarios) {
+      formatarFuncionario(funcionario);
     }
   }
 }
